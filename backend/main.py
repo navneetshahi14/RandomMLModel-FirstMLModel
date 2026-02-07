20283cd7c667bfa6149f1524e2e20afdfe8a7656
navneetshahi14 <navneet.shahi2004@gmail.com>
2026-02-07 23:22:44 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI(title="Placement Prediction API")
+
+model = joblib.load("model.pkl")
+
+
+class StudentData(BaseModel):
+    cgpa: float
+    iq: float
+
+
+@app.get("/")
+def home():
+    return {"message": "Placement Prediction API running 🚀"}
+
+
+@app.post("/predict")
+def predict(data: StudentData):
+    try:
+        features = np.array([[data.cgpa, data.iq]])
+        prediction = model.predict(features)[0]
+
+        result = "Placement Hoga ✅" if prediction == 1 else "Placement Nahi Hoga ❌"
+
+        return {
+            "cgpa": data.cgpa,
+            "iq": data.iq,
+            "prediction": int(prediction),
+            "result": result
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
